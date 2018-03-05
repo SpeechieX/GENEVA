877545302a881057ad24dbea3f0b45cea1ab9cd6
Erik Robertson <writeerikh@gmail.com>
2018-03-05 09:57:14 -0800

    navigation tweaks

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 var User = require('../../models/user');
 var usersCtrl = require('../../controllers/users');
+// var swipe = require('../../src/utils/userService');
 
 /*---------- Public Routes ----------*/
 router.post('/signup', usersCtrl.signup);
 router.post('/login', usersCtrl.login);
+// router.post('/logout', swipe.logout);
 
 
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -19,14 +19,26 @@ class NavBar extends Component {
 
 render() {
     return (
-        <div className='Nav'>
-          <div className="NavList">
-            <Link to="/home"><li>HOME</li></Link>
-            { this.props.user && <Link to="/video"><li>README</li></Link> }
-            <Link to="/signup"><li>SIGN UP</li></Link>
-            <Link to="/login"><li>LOGIN</li></Link>
-          </div>
-        </div>
+        // <div className='Nav'>
+        //   <div className="NavList">
+        //     <Link to="/home"><li>HOME</li></Link>
+        //     { this.props.user && <Link to="/video"><li>README</li></Link> }
+        //     <Link to="/signup"><li>SIGN UP</li></Link>
+        //     <Link to="/login"><li>LOGIN</li></Link>
+        //   </div>
+        // </div>
+      <div className="navbar bg-dark navbar-light">
+        <nav class="navbar navbar-light bg-faded">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          </button>
+        <a class="navbar-brand" href="#">GENEVA</a>
+        <a class="nav-link" href="/home">Connect <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/signup">Account <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/login">Login <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/logout">Logout <span class="sr-only">(current)</span></a>
+        </nav>
+      </div>
+     
     );
   }
 };
